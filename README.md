30799dae7ca7e3500a72af3468820ec5f40da766
aguinaldobrga <guigo.abf@gmail.com>
2025-08-26 15:26:24 -0400

    meu primeiro projeto java

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # dio-java-basico
 Repositório para armazenar todo o conteúdo do Java básico do curso da DIO.
+
---
+++ meu-primeiro-java/src/App.java
@@ -0,0 +1,6 @@
+public class App {
+  
+    public static void main(String[] args) throws Exception {
+        System.out.println("É nos na teclogia Java com a dio!");
+    }
+}
